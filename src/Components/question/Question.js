78751461a48800a6c4b0013d217700f7a6ee1b32
ajhridoy <ajhridoy461@gmail.com>
2022-10-11 21:15:26 +0600

    use shadow to each quiz div

--- src/Components/question/Question.js
+++ src/Components/question/Question.js
@@ -29,7 +29,7 @@ const Question = ({ques}) => {
         })
     }
     return (
-        <div className='bg-slate-100 w-3/4 mx-auto p-4 my-8 rounded-lg'>
+        <div className='bg-slate-300 w-3/4 mx-auto p-4 my-8 rounded-lg shadow-lg'>
             <div className='flex justify-between mb-4'>
             <h2 className='text-xl font-semibold w-full'>{parts1[1].split('</p>')}</h2>
             <button className='basis-1/12' onClick={eyeBtnHandler} title="See Correct Ans"><EyeIcon className="h-6 w-6 text-gray-500 hover:text-blue-500 ml-4"/></button>
