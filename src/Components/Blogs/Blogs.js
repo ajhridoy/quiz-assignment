15a55987e45f76c067bee1eef4c0b370530f1196
ajhridoy <ajhridoy461@gmail.com>
2022-10-11 17:30:12 +0600

    added first ques in blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>This is blog</h2>
+            <div className='bg-orange-300 m-10 p-6 rounded-lg'>
+                <h2 className='text-2xl font-bold mb-4'>What is the Purpose of React Router?</h2>
+                <p className='text-lg'>React Router allows us to build a single-page web application with navigation without the page refreshing as the user navigates. React Router uses component structure to call components, which display the appropriate information.By preventing a page refresh, and using Router or Link, which is explained in more depth below, the flash of a white screen or blank page is prevented. This is one increasingly common way of having a more seamless user experience.</p>
+            </div>
         </div>
     );
 };
