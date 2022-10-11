a278908173ee1ef3e1f49ef2c8c3327b167ebbf8
ajhridoy <ajhridoy461@gmail.com>
2022-10-11 17:09:39 +0600

    added tiny chart in statistic routr

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Statistics = () => {
+    const loadData = useLoaderData().data
+    console.log(loadData)
     return (
+        
         <div>
-            <h2>Thgis is statistcs</h2>
+            <h2 className='text-xl font-bold mt-4'>This graph shows how many quizzes each topic has!!</h2>
+                 <div className='w-full md:w-2/4 lg:mx-auto my-10'>
+                 <BarChart width={500} height={400} data={loadData}>
+                    <Bar dataKey="total" fill="#8884d8" />
+                    <XAxis dataKey="name"></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                </BarChart>
+                 </div>
+  
         </div>
     );
 };
